f7cdadb9f92abde8d2b52139d89591f348e7dc0e
yves <musanajyvesm@gmail.com>
2026-09-25 11:36:15 +0200

    End Of Login And SignUp Api

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -1,13 +1,17 @@
-import User from "../models/userModel";
+import { generateToken } from "../lib/utils.js";
+import User from "../models/userModel.js";
 import bcrypt from "bcryptjs";
 
 export const signup = async (req, res) => {
     const { fullName, email, password } = req.body;
     try {
+        if (!fullName || !email || !password) {
+            return res.status(400).json({ message: "Please provide all required fields" });
+        }
         if (password.length < 6) {
             return res.status(400).json({ message: "Password must be at least 6 characters long" });
         }
-        const user = User.findOne({ email });
+        const user = await User.findOne({ email });
         if (user) {
             return res.status(400).json({ message: "User already exists" });
         }
@@ -19,16 +23,57 @@ export const signup = async (req, res) => {
             email: email,
             password: hashedPassword
         });
-        await newUser.save();
+
+        if (newUser) {
+            generateToken(newUser._id, res);
+            await newUser.save();
+            return res.status(201).json({
+                id: newUser._id,
+                fullName: newUser.fullName,
+                email: newUser.email,
+                profilePicture: newUser.profilePicture || "",
+                message: "User created successfully"
+            });
+        } else {
+            return res.status(400).json({ message: "Invalid user data" });
+        }
     } catch (error) {
-        return res.status(500).json({ message: "Internal server error" });
+        console.error("Error signing controller:", error.message);
+        return res.status(500).json({ message: "Server error" });
     }
 };
 
 export const login = async (req, res) => {
-    res.send("Login controller");
+    const { email, password } = req.body;
+    try {
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ message: "Invalid email or password" });
+        }
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+            return res.status(400).json({ message: "Invalid email or password" });
+        }
+        generateToken(user._id, res);
+
+        return res.status(200).json({
+            id: user._id,
+            fullName: user.fullName,
+            email: user.email,
+            profilePicture: user.profilePicture || ""
+        });
+    } catch (error) {
+        console.error("Error logging in controller:", error.message);
+        return res.status(500).json({ message: "Server error" });
+    }
 };
 
 export const logout = (req, res) => {
-    res.send("Logout controller");
+    try {
+        res.cookie("jwt", "", { maxAge: 0 });
+        return res.status(200).json({ message: "Logged out successfully" });
+    } catch (error) {
+        console.error("Error logging out controller:", error.message);
+        return res.status(500).json({ message: "Server error" });
+    }
 };
